f6bd24e4195307513320e89513dac1e1b6eb7d9c
Draguve <ritwikpatil1998@gmail.com>
2018-09-09 02:00:13 +0530

    Added functions to spotify.py added vscode to gitignore

--- api/spotify/__init__.py
+++ api/spotify/__init__.py
@@ -5,12 +5,19 @@ from spotipy.oauth2 import SpotifyClientCredentials
 def get_random_playlists(limit=1,offset=-1):
     if(offset==-1):
         offset = random.randint(0,1500)
-    client_credentials_manager = SpotifyClientCredentials(client_id, client_secret)
-    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
-    playlists = sp.user_playlists('spotify',limit=limit,offset=offset)
-    return playlists
+    return get_playlist('spotify',limit=limit,offset=offset)
 
 def get_token():
     scope = 'playlist-modify-private'
     token = util.prompt_for_user_token(username,scope,client_id=client_id,client_secret=client_secret,redirect_uri=redirect_uri)
     return token
+
+def get_playlist(user,limit,offset):
+    client_credentials_manager = SpotifyClientCredentials(client_id, client_secret)
+    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+    playlists_json = sp.user_playlists(user,limit=limit,offset=offset)
+    return playlists_json
+
+def get_playlist_content(user,playlistid=None,fields=None):
+    sp = spotipy.Spotify(auth=get_token())
+    return sp.user_playlist(user,playlist_id=playlistid,fields=fields)
