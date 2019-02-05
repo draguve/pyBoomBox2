b66a8743ec11a8a86b82344df5c052b5c70397d3
Draguve <ritwikpatil1998@gmail.com>
2019-02-05 16:34:16 +0530

    fixed dependencies

--- api/app.py
+++ api/app.py
@@ -8,10 +8,10 @@ from client import client
 
 app = Flask(__name__)
 
-app.register_blueprint(admin_panel,url_prefix="/admin")
-app.register_blueprint(client,url_prefix="/api")
+app.register_blueprint(admin_panel, url_prefix="/admin")
+app.register_blueprint(client, url_prefix="/api")
 
-#let this code stay here for future refrence
+# let this code stay here for future reference
 # @app.route('/add/<int:param1>/<int:param2>')
 # def add(param1: int, param2: int) -> str:
 #     task = celery.send_task('tasks.add', args=[param1, param2], kwargs={})
