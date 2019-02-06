459b41021725323bf7f8cca65009173d137d81f3
Draguve <ritwikpatil1998@gmail.com>
2019-02-07 01:43:02 +0530

    fixed oauth issuses , need to fix spotipy lib

--- api/admin_panel/__init__.py
+++ api/admin_panel/__init__.py
@@ -9,9 +9,40 @@ from flask import flash
 from flask import send_from_directory
 from flask import current_app
 
+from worker import celery
+import celery.states as states
 
 admin_panel = Blueprint('admin_panel', __name__, static_folder='static', template_folder='templates')
+
 
 @admin_panel.route('/')
 def index():
     return "Test"
+
+
+@admin_panel.route('/get_url')
+def get_url():
+    task = celery.send_task('auth.get_url')
+    response = f"<a href='{url_for('admin_panel.check_task', task_id=task.id, external=True)}'>check status of {task.id} </a>"
+    return response
+
+
+@admin_panel.route('/auth_url', methods=['POST', 'GET'])
+def auth_url():
+    if request.method == 'POST':
+        url = request.form['response']
+        task = celery.send_task('auth.response_url', args=[url, ], kwargs={})
+        response = f"<a href='{url_for('admin_panel.check_task', task_id=task.id, external=True)}'>check status of {task.id} </a>"
+        return response
+    else:
+        return render_template('auth_url_form.j2')
+
+
+@admin_panel.route('/check/<string:task_id>')
+def check_task(task_id: str) -> str:
+    res = celery.AsyncResult(task_id)
+    if res.state == states.PENDING:
+        return res.state
+    else:
+        return str(res.result)
+
--- celery-queue/spotify/models.py
+++ celery-queue/spotify/models.py
@@ -1,20 +1,21 @@
 from . import *
 
+
 class Track:
-    def __init__(self,json,is_playlist_track=False):
-        if(is_playlist_track):
+    def __init__(self, json, is_playlist_track=False):
+        if is_playlist_track:
             json = json['track']
         self.id = json['id']
         self.title = json['name']
         self.album = json['album']['name']
 
-        #song might have multiple artist
+        # song might have multiple artist
         self.artist = []
         self.artistids = []
         for artist in json['artists']:
             self.artist.append(artist['name'])
             self.artistids.append(artist['id'])
-        
+
         # images 1 contains 300px X 300px images
         self.albumart = json['album']['images'][1]['height']
         self.explict = json['explicit']
@@ -22,20 +23,21 @@ class Track:
         self.url = json['external_urls']['spotify']
         self.popularity = json['popularity']
 
+
 class Artist:
-    def __init__(self,json,simplified):
+    def __init__(self, json, simplified):
         self.simplified = simplified
         self.id = json['id']
         self.name = json['name']
-        if(not(simplified)):
-            self.desimplify(self,json)
+        if not simplified:
+            self.desimplify(self, json)
 
-    def desimplify(self):
-        if(self.simplified):
-            #get the complete artist
-            desimplify(self,json)
+    # def desimplify(self):
+    #     if(self.simplified):
+    #         #get the complete artist
+    #         desimplify(self,json)
 
-    def desimplify(self,json):
+    def desimplify(self, json):
         self.genres = json['genres']
         self.popularity = json['popularity']
 
@@ -43,15 +45,15 @@ class Artist:
         for image in json['images']:
             self.images.append(image['url'])
 
+
 class Playlist:
-    def __init__(self,json):
+    def __init__(self, json):
         self.id = json['id']
         self.name = json['name']
         self.no_of_tracks = json['tracks']['total']
         self.username = json['owner']['display_name']
         self.tracks = None
-    
-    def get_tracks(self):
-        self.tracks = get_playlist_content(user=self.username,playlistid=self.id)
-        return self.tracks
 
+    def get_tracks(self):
+        self.tracks = get_playlist_content(user=self.username, playlistid=self.id)
+        return self.tracks
