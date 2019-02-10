7963c4451c4da6e6309d275709f1dab7801e7547
Rishi Kaushik <rishikaushik60@gmail.com>
2019-02-10 22:36:43 +0530

    made RequestTypes
    each request must contain a request type to tell what this request is about, rest of the structure of the request is according to the request type

--- api/client/__init__.py
+++ api/client/__init__.py
@@ -8,20 +8,21 @@ from flask import url_for
 from flask import flash
 from flask import send_from_directory
 from flask import current_app
+from .requestTypes import RequestTypes
 
 client = Blueprint('client', __name__, static_folder='static', template_folder='templates')
 
-
-@client.route('/login', methods=['GET', 'POST'])
+@client.route('/', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         if request.is_json:
-            return str(request.get_json()['mytext']) + " is a biatch"
+            if request.get_json()['type'] == RequestTypes.addVote:
+                pass
         else:
             return request.form['firstname'] + " is a bitch"
     else:
         return """
-        <form action="/api/login"  method="post">
+        <form action="/api"  method="post">
             First name:<br>
             <input type="text" name="firstname" value=""><br>
             Last name:<br>
