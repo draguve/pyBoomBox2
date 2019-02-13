015bf102f344b05341d9b6d90d35231f284c47a6
Rishi Kaushik <rishikaushik60@gmail.com>
2019-02-13 12:48:30 +0530

    added add_vote

--- api/client/__init__.py
+++ api/client/__init__.py
@@ -17,38 +17,51 @@ import json
 import random
 
 client = Blueprint('client', __name__, static_folder='static', template_folder='templates')
-REDIS_URL = os.environ.get('REDIS_BROKER_URL', 'redis://localhost:6379/0')
+REDIS_URL = os.environ.get('REDIS_BROKER_URL', 'redis://192.168.137.54:7777/0')
 
+# def test():
+#     task = celery.send_task('tasks.add')
+#     response = f"<a href='{url_for('check_task', task_id=task.id, external=True)}'>check status of {task.id} </a>"
+#     return True
 
-@client.route('/test/')
-def test():
-    task = celery.send_task('spotify.create_playlist')
-    return "Done"
+def add_new_id():
+    new_id = random.getrandbits(128)
+    while valid(new_id):
+        new_id = random.getrandbits(128)
+    id_list = redis_get('id_list')
+    id_list.update(new_id)
+    redis_set('id_list',id_list)
+    return new_id
+
+# TODO: test when redis server available
+def add_vote(song:str):
+    song_list = redis_get('song_list')
+    if song in song_list:
+        song_list['song'] += 1
+        return json.dumps({'status' : 'vote added'})
+    else:
+        return json.dumps({'status' : 'song not up for voting'})
+
 
 @client.route('/', methods=['GET', 'POST'])
 def request_handler():
     if request.method == 'POST':
         if request.is_json:
             req = request.get_json()
-            if req['type'] == RequestTypes.add_client:
-                new_id = random.getrandbits(128)
-                while not valid(new_id):
-                    new_id = random.getrandbits(128)
-                add_new_id(new_id)
+            if req['type'] == RequestTypes.add_client.value: # vulnerable to DOS
+                new_id = add_new_id()
                 result = {
                     'status' : 'client created',
                     'id' : new_id
                 }
                 return json.dumps(result)
-            
-            if valid(req['id']):
-                if req['type'] == RequestTypes.add_vote:
-                    pass
+            elif valid(req['id']):
+                if req['type'] == RequestTypes.add_vote.value:
+                    return add_vote(req['song'])
         else:
             return 'only json objects accepted'
     else:
         return 'invalid'
-
 
 def get_redis_db():
     redis_db = getattr(g, '_redis', None)
@@ -56,28 +69,13 @@ def get_redis_db():
         redis_db = g._redis = redis.from_url(REDIS_URL)
     return redis_db
 
-@client.route('/t/')
-def tst():
-    return 'ass'
-
-@client.route('/test/')
-def index():
-    value = {
-        'a':'A',
-        'b':'B'
-    }
-    redis_set('key',value)
-    return str(redis_get('key'))
-
-# FOR REFERENCE
 def redis_set(key:str, val):
     value = json.dumps(val)
     get_redis_db().set(key,value)
 
-@client.route('/redis_get/')
-def redis_get():
-    x = get_redis_db().get('test')
-    return x
+def redis_get(key:str):
+    value = get_redis_db().get(key)
+    return json.loads(value)
 
 def valid(id:str):
     id_list = redis_get('id_list')
@@ -86,4 +84,4 @@ def valid(id:str):
     else:
         return False
 
-# requests.post('http://localhost:5000/api/add_message/1234', json={"mytext":"lalala"})
+# requests.post('http://localhost:5000/api/', json={"type":1})
